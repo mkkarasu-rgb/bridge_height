5f657da5e9ca9434643f1edcba4d1ce2e9e830dc
Kemal Karasu <mkkarasu@gmail.com>
2025-09-05 17:05:27 +0300

    a

--- new.py
+++ new.py
@@ -27,24 +27,25 @@ if page == "New Obstacle":
 
     if selected_method == "Select on Map":
 
-        col1, col2 = st.columns(2)
-        col1.text_input("Enter obstacle name:", key="obstacle_name")
-        col2.text_input("Enter obstacle height in meters:", key="obstacle_height")
-    
-        location = get_geolocation()
-        coords = location["coords"] if location and "coords" in location else {}
-        lat = coords.get("latitude")
-        lon = coords.get("longitude")
-        if lat is not None and lon is not None:
-            m = folium.Map(location=[lat, lon], zoom_start=12)
-            folium.Marker([lat, lon], popup="You are here", icon=folium.Icon(color="blue")).add_to(m) 
-        m.add_child(folium.LatLngPopup())
-        map_data = st_folium(m, height=500, width=800)
-        if map_data and "last_clicked" in map_data and map_data["last_clicked"]:
-            lat = map_data["last_clicked"]["lat"]
-            lon = map_data["last_clicked"]["lng"]
-        else:
-            lat, lon = None, None
+        with st.expander("Instructions", expanded=False):
+            col1, col2 = st.columns(2)
+            col1.text_input("Enter obstacle name:", key="obstacle_name")
+            col2.text_input("Enter obstacle height in meters:", key="obstacle_height")
+        
+            location = get_geolocation()
+            coords = location["coords"] if location and "coords" in location else {}
+            lat = coords.get("latitude")
+            lon = coords.get("longitude")
+            if lat is not None and lon is not None:
+                m = folium.Map(location=[lat, lon], zoom_start=12)
+                folium.Marker([lat, lon], popup="You are here", icon=folium.Icon(color="blue")).add_to(m) 
+            m.add_child(folium.LatLngPopup())
+            map_data = st_folium(m, height=500, width=800)
+            if map_data and "last_clicked" in map_data and map_data["last_clicked"]:
+                lat = map_data["last_clicked"]["lat"]
+                lon = map_data["last_clicked"]["lng"]
+            else:
+                lat, lon = None, None
 
     elif selected_method == "Enter Address or Coordinates":
 
