3882a0aa363b6ccdb0f98134c2f3917a56c44eeb
Kemal Karasu <mkkarasu@gmail.com>
2025-09-05 18:43:47 +0300

    a

--- new.py
+++ new.py
@@ -50,8 +50,10 @@ if page == "New Obstacle":
         if lat is not None and lon is not None:
             m = folium.Map(location=[lat, lon], zoom_start=15)
             folium.Marker([lat, lon], popup=address).add_to(m)
-        m.add_child(folium.LatLngPopup())
-        map_data = st_folium(m, height=500, width=800)
+            m.add_child(folium.LatLngPopup())
+            map_data = st_folium(m, height=500, width=800)
+        else:
+            map_data = None
         if map_data and "last_clicked" in map_data and map_data["last_clicked"]:
             lat = map_data["last_clicked"]["lat"]
             lon = map_data["last_clicked"]["lng"]
